055965d2bdd125cd51c7336cb057bd6002835cd8
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-02-19 09:00:59 -0500

    done

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ def get_playlist():
     for playlist  in results:
         playlist_dict = {
             "id": playlist.Playlist.id,
+            "name": playlist.Playlist.name,
             "artist_id": playlist.Artist.name,
-            "song_id": playlist.Song.name
+            "song_id": playlist.Song.name, 
         }
         playlist_list.append(playlist_dict)
     return playlist_list
@@ -45,10 +46,45 @@ async def add_song(name: str):
     return {'song added': new_song.name}
 
 @app.post('/add/playlist')
-async def add_playlist(name: str, artist_id: int, song_id: int):
-    new_playlist = Playlist(name = name, artist_id = artist_id, song_id = song_id)
+async def add_playlist(id: int, name: str, artist_id: int, song_id: int):
+    new_playlist = Playlist(id = id, name = name, artist_id = artist_id, song_id = song_id)
     session.add(new_playlist)
     session.commit()
     return {'playlist added': new_playlist} 
+
+@app.delete('/artist/{id}/delete')
+async def remove_artist(id: int):
+    artist = session.query(Artist).filter(Artist.id == id).first()
+    if artist is not None:
+        session.query(Playlist).filter(Playlist.artist_id == id).delete()
+        session.delete(artist)
+        session.commit()
+        return {"Deleted Artist": artist.name}
+
+@app. delete('/song/{id}/delete') 
+async def remove_song(id: int):
+    song = session.query(Song).filter(Song.id == id).first()
+    if song is not None:
+        session. query (Playlist).filter(Playlist.song_id == id).delete()
+        session.delete(song)
+        session.commit() 
+        return {"Deleted song": song.name}
+    
+@app.put('/artist/{id}/update')
+async def update_artist(id: int, new_name: str):
+    artist = session.query(Artist).filter(Artist.id == id).first()
+    if artist is not None:
+        artist.name = new_name
+        session.commit()
+        return {"Updated student": {"id": artist.id, "name":artist.name}}
+    
+@app.put('/song/{id}/update')
+async def update_song(id: int, new_name: str):
+    song = session.query(Song).filter(Song.id == id).first()
+    if song is not None:
+        song.name = new_name
+        session.commit() 
+        return {"Updated student": {"id": song.id, "name": song.name}}
+
 
 
